8c17aa72fc6f4ff8d7a06ad4a5d9da74e0d7f2b5
Andreson Souza <andresonjazz@gmail.com>
2020-11-16 16:53:24 -0300

    feat: use os operadores para ordenar a lista de acoes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  // api: 'http://localhost:3000',
-  api: 'http://10.0.0.105:3000',
+  api: 'http://localhost:3000',
   imagesPath: 'assets',
   name: 'Byte Bank Broker',
 };
